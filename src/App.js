a85972b2c62e586b2d97c8fb8080e6a70bfa88b8
Chris Athanas <chris@crowdskout.com>
2021-02-01 22:26:14 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { Count, CountMemoized } from "./Count"
+
+// How to use memo in React to Optimize Renders
+// https://www.youtube.com/watch?v=o-alRbk_zP0
 
 function App() {
+  const [text, setText] = React.useState("");
+
+  // const onEven = React.useCallback( () => setDataEven(true)  )
+  function onEven() { setDataEven(true) }
+  const onOdd = React.useCallback( () =>  setDataEven(false) );
+  // const resetTextCallback = React.useCallback( () => setText("") );
+  const resetTextCallback = () => { setText("") }
+  
+  const data = { isEven: false};
+
+  function setDataEven(value) {
+    data.isEven = value
+    console.log("setDataEven=", data.isEven)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value = {text} onChange={e => setText(e.target.value)} />
+      <Count onEven={onEven} onOdd={onOdd} setText={setText} resetTextCallback={resetTextCallback} data={data} />
+
     </div>
   );
 }
 
 export default App;
+
+
+
+// Note: This Count causes component to re-render due to 'data' object being recreated with {}'s
+// function App() {
+//   const [text, setText] = React.useState("");
+
+//   const onOdd = React.useCallback( () => setText(""), [setText]);
+//   const data = { isEven: false};
+
+//   return (
+//     <div className="App">
+//       <input value = {text} onChange={e => setText(e.target.value)} />
+//       <Count onOdd={onOdd} data={data} />
+//     </div>
+//   );
+// }
+
+
+
+// NOTE: This Count component causes re-renders due to new onOdd anon function being created with {}'s
+// function App() {
+//   const [text, setText] = React.useState("");
+
+//   const onOdd = React.useCallback( () => setText(""), [setText]);
+//   const data = { isEven: false};
+
+//   return (
+//     <div className="App">
+//       <input value = {text} onChange={e => setText(e.target.value)} />
+//       <Count 
+//         onOdd={ () => {
+//           setText("")
+//         }}
+//       /> 
+
+//     </div>
+//   );
+// }
---
+++ src/Count.js
@@ -0,0 +1,110 @@
+import React, { useEffect } from "react";
+
+export const Count = React.memo(( { onEven, onOdd, setText, resetTextCallback, ...props }) => {
+  const [count, setCount] = React.useState(0);
+  const renders = React.useRef(0);
+
+  useEffect( () => {
+    console.log(props.data)
+    // if (props.data.isEven) setText("")
+    if (props.data.isEven) resetTextCallback()
+  }, [props.data.isEven])
+
+  return (
+    <div className="App">
+      <div>count: { count }</div>
+      <div>renders: { renders.current++ }</div>
+      <button
+        onClick={ () => {
+          setCount(c => {
+            c = c + 1
+            if ( c % 2 === 0) {
+              onEven()  // clear text only when count is even
+              // props.data.isEven = true
+            } else {
+              onOdd()
+              // props.data.isEven = false
+            }
+            return c
+          })
+        }}
+        >
+          increment
+        </button>
+    </div>
+  )
+}, (prevProps, thisProps) => {
+  // deep check of props objects
+  if(prevProps.data.isEven !== thisProps.data.isEven) {
+    return true; // do a re-render
+  }
+  return false; // dont re-render
+});
+
+
+// Only re-renders when the props or state changes
+export const CountMemoized = React.memo(() => {
+  const [count, setCount] = React.useState(0);
+  const renders = React.useRef(0);
+
+  return (
+    <div className="App">
+      <div>countMemoized: {count}</div>
+      <div>rendersMemoized: {renders.current++}</div>
+      <button
+        onClick={ () => {
+          setCount(c => c+1);
+        }}
+        >
+          increment
+        </button>
+    </div>
+  )
+});
+
+// Causes rerenders bc its a child component of parent, and always renders when parent state changes
+export function CountAlwaysRendersWithParent() {
+  const [count, setCount] = React.useState(0);
+  const renders = React.useRef(0);
+
+  return (
+    <div className="App">
+      <div>count: {count}</div>
+      <div>renders: {renders.current++}</div>
+      <button
+        onClick={ () => {
+          setCount(c => c+1);
+        }}
+        >
+          increment
+        </button>
+    </div>
+  )
+};
+
+
+
+
+// This causes re-renders when the 'data' prop object is passed in (bc value is not deep checked)
+//
+// export const Count = React.memo(({ onOdd }) => {
+//   const [count, setCount] = React.useState(0);
+//   const renders = React.useRef(0);
+
+//   return (
+//     <div className="App">
+//       <div>count: {count}</div>
+//       <div>renders: {renders.current++}</div>
+//       <button
+//         onClick={ () => {
+//           if (count % 2 === 0) {
+//             onOdd();
+//           }
+//           setCount(c => c+1);
+//         }}
+//         >
+//           increment
+//         </button>
+//     </div>
+//   )
+// });  //<-- NOTE! lack of memo deep check func
